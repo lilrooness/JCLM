2051ae9572803c166319b95de9b05f442c6a5511
lilrooness <jfrangoudes@gmail.com>
2012-11-01 18:15:04 +0000

    Folder Structure

--- src/test/Controller.java
+++ src/test/Controller.java
@@ -1,6 +1,5 @@
 public class Controller implements OptionController{
 	public void optionSelected(CMenu menu, String option){
-		System.out.println("Called!!!");
 		System.out.println(option);
 	}
 }
